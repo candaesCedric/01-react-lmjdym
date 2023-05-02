589db128d667801d5ebcab3268b0a2e4bb96b0d1
candaesCedric <118951399+candaesCedric@users.noreply.github.com>
2023-05-02 14:39:42 +0200

    exo fini?

--- src/index.js
+++ src/index.js
@@ -4,13 +4,42 @@ import './style.css';
 
 export const App = (
   // write your React Code here
-
   <div>
     <h1 className="text-center">My contacts list 📱</h1>
-
-    <div>
-      // add at least 5 people contact info (name, phone, email and a delete
-      button)
+    <div className="person-item">
+      <h3>paul emploa</h3>
+      <h4>
+        oé@pnj.fr <br /> 06-05-04-03-02
+      </h4>
+      <button>Delete</button>
+    </div>
+    <div className="person-item">
+      <h3>j'vepa enfercink</h3>
+      <h4>
+        banan@oula.fr <br /> 06-05-04-03-01
+      </h4>
+      <button>Delete</button>
+    </div>
+    <div className="person-item">
+      <h3>moajve unconstructor</h3>
+      <h4>
+        unconstructor@Cémieu.oé <br /> 06-06-04-03-01
+      </h4>
+      <button>Delete</button>
+    </div>
+    <div className="person-item">
+      <h3>j'vepa enfercinq</h3>
+      <h4>
+        banan@oula.fr <br /> 06-05-04-03-01
+      </h4>
+      <button>Delete</button>
+    </div>
+    <div className="person-item">
+      <h3>cell der</h3>
+      <h4>
+        fini@finito.fin <br /> 06-05-04-00-01
+      </h4>
+      <button>Delete</button>
     </div>
   </div>
 );
