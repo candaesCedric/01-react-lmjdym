05cdc823df2f3e684fbce64d223d076f60190359
candaesCedric <118951399+candaesCedric@users.noreply.github.com>
2023-05-02 14:13:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 01-react-lmjdym
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/01-react-challange-jku6nu)
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+export const App = (
+  // write your React Code here
+
+  <div>
+    <h1 className="text-center">My contacts list 📱</h1>
+
+    <div>
+      // add at least 5 people contact info (name, phone, email and a delete
+      button)
+    </div>
+  </div>
+);
+
+ReactDOM.render(App, document.getElementById('root'));
